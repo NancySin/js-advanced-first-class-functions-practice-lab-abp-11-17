6abb7a0eb42d1554c8cf86d753ce44965c2007cf
nancy sin <nsin@sbcglobal.net>
2017-12-15 20:04:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,19 +1,5 @@
 // Code your solution in this file!
-const logDriverNames = function(drivers){
-  drivers.forEach(function (driver) {
-    console.log(driver.name)
-  });
-}
-const logDriversByHometown = function (drivers, hometown)
-{ drivers.forEach(function(driver)
-  { if(driver.hometown === hometown){
-  console.log(driver.name)}
-})
-}
-const driversByRevenue(drivers) = function (drivers) {
-  return drivers.slice().sort(function (driverOne, driverTwo) {
-    return driverOne.revenue - driverTwo.revenue})
-}
+
 
 
 const driversByName = function (drivers){
