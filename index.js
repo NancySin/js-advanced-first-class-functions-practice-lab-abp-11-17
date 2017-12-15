ca9cd151febc392d671ff67dd1b072df01bd1a55
nancy sin <nsin@sbcglobal.net>
2017-12-15 19:45:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,10 @@ const logDriverNames = function(drivers){
     console.log(driver.name)
   });
 }
+const logDriversByHometown =Function (drivers, hometown)
+{ drivers.forEach(function(drivers)
+  { if(driver.hometown ===h ometown){
+  console.log(driver.name)}
+})
+}
 
