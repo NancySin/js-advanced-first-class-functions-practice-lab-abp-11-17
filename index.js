e181dfba754c032629354cfae07fd6d546e2ac9f
nancy sin <nsin@sbcglobal.net>
2017-12-15 19:52:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,3 @@ const logDriversByHometown = function (drivers, hometown)
   console.log(driver.name)}
 })
 }
-const driversByRevenue(drivers)= function (drivers){
-  return drivers.slice().sort(function(driverOne, driverTwo){
-    return driverOne.revenue-driverTwo.revenue})
-}
