f00b473bb7b6aea76252913143a9183e437432e6
nancy sin <nsin@sbcglobal.net>
2017-12-15 19:21:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-// Code your solution in this file!
-function logDriverNames(drivers){drivers.forEach(function(driver)
-console.log(driver.name)})
+
+function logDriversByHometown(drivers, hometown){ drivers.forEach(function(driver)
+  { if(driver.hometown ===hometown)
+  (console.log(driver.name)}
+})
+}
+
+function driversByRevenue(driver){
+  return drivers.slice().sort(function(a,b){
+    return a.revenue-b.revenue})
 }
