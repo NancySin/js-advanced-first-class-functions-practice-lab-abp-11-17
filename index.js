ea0d65ddd91136808b4fca459c25eea216be6e8e
nancy sin <nsin@sbcglobal.net>
2017-12-15 20:04:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 // Code your solution in this file!
-
-
-
-const driversByName = function (drivers){
-  return drivers.slice().sort(function(driverOne, driverTwo){
-    return driveOne.name.localeCompare(driverTwo.name)
-  })
+const logDriverNames = function(drivers){
+  drivers.forEach(function (driver) {
+    console.log(driver.name)
+  });
+}
+const logDriversByHometown = function (drivers, hometown)
+{ drivers.forEach(function(driver)
+  { if(driver.hometown === hometown){
+  console.log(driver.name)}
+})
 }
